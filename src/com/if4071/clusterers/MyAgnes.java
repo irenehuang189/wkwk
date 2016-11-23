1a2e0fd06155956fe69bff2def1e310ae7657425
irenehuang189 <irenehuang189@gmail.com>
2016-11-24 06:33:43 +0700

    Finalize iteration to build new cluster

--- src/com/if4071/clusterers/MyAgnes.java
+++ src/com/if4071/clusterers/MyAgnes.java
@@ -1,6 +1,5 @@
 package com.if4071.clusterers;
 
-import com.if4071.classifiers.trees.MyNode;
 import weka.core.DistanceFunction;
 import weka.core.EuclideanDistance;
 import weka.core.Instances;
@@ -53,26 +52,87 @@ public class MyAgnes {
 
     public void buildClusterer() {
         initDistances();
-        printDistances();
 
-        MyPairDistance nearestPairDistance = getNearestInitDistances();
-        System.out.println(nearestPairDistance.toString());
+        MyPairDistance nearestPairDistance = getNearestInitDistances(initDistances);
         tree = new MyAgnesNode(String.valueOf(nearestPairDistance.getDistance()));
+        mergeNearestIndices(nearestPairDistance.getNearestPair());
+        System.out.println(nearestPairDistance.getDistance());
+
+        while(indices.size() > clusterNum) {
+            buildNewCluster();
+        }
+        System.out.println(indices);
     }
 
-    public MyPairDistance getNearestInitDistances() {
-        double minDistance = 99999999;
+    public void buildNewCluster() {
+        double[][] newDistances = new double[indices.size()][indices.size()];
+        for(int i=0; i<indices.size(); i++) {
+            for(int j=0; j<indices.size(); j++) {
+                newDistances[i][j] = getNearestDistanceBetweenClusters(indices.get(i), indices.get(j));
+            }
+        }
+
+        MyPairDistance nearestPairDistance = getNearestInitDistances(newDistances);
+        mergeNearestIndices(nearestPairDistance.getNearestPair());
+        System.out.println(nearestPairDistance.getDistance());
+    }
+
+    public void mergeNearestIndices(ArrayList<Integer> nearestIndices) {
+        Integer destIndex = nearestIndices.get(0);
+        Integer oriIndex = nearestIndices.get(1);
+        indices.get(destIndex).addAll(indices.get(oriIndex));
+        indices.remove((int) oriIndex);
+    }
+
+    public double getNearestDistanceBetweenClusters(ArrayList<Integer> cluster1, ArrayList<Integer> cluster2) {
+        double chosenDistance = 0;
+        switch (linkType){
+            case SINGLE:
+                chosenDistance = 999999999;
+                break;
+            case COMPLETE:
+                chosenDistance = -999999999;
+                break;
+        }
+
+        for(int i=0; i<cluster1.size(); i++) {
+            for(int j=0; j<cluster2.size(); j++) {
+                Integer index1 = cluster1.get(i);
+                Integer index2 = cluster2.get(j);
+                double distance = initDistances[index1][index2];
+                switch (linkType){
+                    case SINGLE:
+                        if(chosenDistance > distance) {
+                            chosenDistance = distance;
+                        }
+                        break;
+                    case COMPLETE:
+                        if(chosenDistance < distance) {
+                            chosenDistance = distance;
+                        }
+                        break;
+                }
+            }
+        }
+
+        return chosenDistance;
+    }
+
+    public MyPairDistance getNearestInitDistances(double[][] distances) {
+        double minDistance = 999999999;
         ArrayList<Integer> nearestPair = new ArrayList<>();
-        for(int i=0; i<initDistances.length; i++) {
-            for(int j=i+1; j<initDistances[i].length; j++) {
-                if(minDistance > initDistances[i][j]) {
-                    minDistance = initDistances[i][j];
+        for(int i=0; i<distances.length; i++) {
+            for(int j=i+1; j<distances[i].length; j++) {
+                if(minDistance > distances[i][j]) {
+                    minDistance = distances[i][j];
                     nearestPair.clear();
                     nearestPair.add(i);
                     nearestPair.add(j);
                 }
             }
         }
+        System.out.println(nearestPair.get(0) + " " + nearestPair.get(1));
+        System.out.println("Pair: " + data.instance(nearestPair.get(0)) + " " + data.instance(nearestPair.get(1)));
 
         return new MyPairDistance(nearestPair, minDistance);
     }
@@ -90,18 +150,17 @@ public class MyAgnes {
         }
     }
 
-    public void printDistances() {
-        int numInstances = data.numInstances();
-        for(int i=0; i<initDistances.length; i++) {
-            for(int j=0; j<initDistances.length; j++) {
-                System.out.print(initDistances[i][j] + " ");
+    public void printDistances(double[][] distances) {
+        for(int i=0; i<distances.length; i++) {
+            for(int j=0; j<distances.length; j++) {
+                System.out.print(distances[i][j] + "    ");
             }
             System.out.println();
         }
     }
 
     public static void main(String[] args) {
-        String fileName = "data/weather.numeric.arff";
+        String fileName = "data/weather.arff";
         try {
             BufferedReader br = new BufferedReader(new FileReader(fileName));
             ArffLoader.ArffReader arffReader = new ArffLoader.ArffReader(br);
