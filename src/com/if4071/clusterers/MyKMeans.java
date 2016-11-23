a7ead744571540e1b6feca84e31f44793504bd39
angelynz95 <angel_lynz95@hotmail.com>
2016-11-23 22:42:34 +0700

    initialize centroids by random

--- src/com/if4071/clusterers/MyKMeans.java
+++ src/com/if4071/clusterers/MyKMeans.java
@@ -1,5 +1,6 @@
 package com.if4071.clusterers;
 
+import weka.clusterers.RandomizableClusterer;
 import weka.core.Instance;
 import weka.core.Instances;
 import weka.core.converters.ArffLoader;
@@ -8,6 +9,7 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Random;
 import java.util.Scanner;
 
 /**
@@ -15,14 +17,27 @@ import java.util.Scanner;
  */
 public class MyKMeans {
     private Instances currentCentroids;
+    private Instances data;
     private int numCluster;
+    private int numData;
 
     public MyKMeans() {
 
     }
 
-    public void buildClusterer(Instances dataset, int numCluster) {
+    public void buildClusterer(Instances data, int numCluster) {
+        this.data = data;
         this.numCluster = numCluster;
+        this.currentCentroids = new Instances(this.data, this.numCluster);
+        this.numData = this.data.numInstances();
+        initializeCentroids();
+    }
+
+    private void initializeCentroids() {
+        int[] centroidIndexes = new Random().ints(0, numData).distinct().limit(numCluster).toArray();
+        for (int i = 0; i < centroidIndexes.length; i++) {
+            currentCentroids.add(data.instance(centroidIndexes[i]));
+        }
     }
 
     public static void main(String[] args) {
@@ -34,12 +49,13 @@ public class MyKMeans {
         try {
             BufferedReader br = new BufferedReader(new FileReader(fileName));
             ArffLoader.ArffReader arffReader = new ArffLoader.ArffReader(br);
-            Instances dataset = arffReader.getData();
+            Instances data = arffReader.getData();
 
-            System.out.print("Masukkan jumlah cluster: ");
-            numCluster = scanner.nextInt();
-            scanner.nextLine();
-            myKMeans.buildClusterer(dataset, numCluster);
+//            System.out.print("Masukkan jumlah cluster: ");
+//            numCluster = scanner.nextInt();
+//            scanner.nextLine();
+            numCluster = 2;
+            myKMeans.buildClusterer(data, numCluster);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
