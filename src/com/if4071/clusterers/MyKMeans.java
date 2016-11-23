2e3a63e422abcb50ff7aa948ef6f774b9de8d6a7
angelynz95 <angel_lynz95@hotmail.com>
2016-11-23 21:22:14 +0700

    create main for MyKMeans

--- src/com/if4071/clusterers/MyKMeans.java
+++ src/com/if4071/clusterers/MyKMeans.java
@@ -1,12 +1,49 @@
 package com.if4071.clusterers;
 
+import weka.core.Instance;
+import weka.core.Instances;
+import weka.core.converters.ArffLoader;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
 /**
  * Created by TOSHIBA on 23-Nov-16.
  */
 public class MyKMeans {
-    
+    private Instances currentCentroids;
+    private int numCluster;
+
+    public MyKMeans() {
+
+    }
+
+    public void buildClusterer(Instances dataset, int numCluster) {
+        this.numCluster = numCluster;
+    }
 
     public static void main(String[] args) {
+        int numCluster;
+        MyKMeans myKMeans = new MyKMeans();
+        Scanner scanner = new Scanner(System.in);
+        String fileName = "data/weather.nominal.arff";
 
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(fileName));
+            ArffLoader.ArffReader arffReader = new ArffLoader.ArffReader(br);
+            Instances dataset = arffReader.getData();
+
+            System.out.print("Masukkan jumlah cluster: ");
+            numCluster = scanner.nextInt();
+            scanner.nextLine();
+            myKMeans.buildClusterer(dataset, numCluster);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
